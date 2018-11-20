66ceae55e4cd841dcf7df9dc13a03431720678de
Midka <cebepnight@gmail.com>
2018-11-20 16:53:48 +0200

    new functions

--- src/lv/ctco/notepad/Main.java
+++ src/lv/ctco/notepad/Main.java
@@ -1,41 +1,135 @@
 package lv.ctco.notepad;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.File;
+import java.util.*;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static List<Person> records = new ArrayList<>();
+   // private static final File RESULTS_FILE = new File("persons.txt");
 
     public static void main(String[] args) {
+        System.out.println("Notepad application.\n--------------==============--------------");
+        showHelp();
+
         for(;;){
-            System.out.println("cmd:");
+            System.out.println("Please, enter command:");
             String cmd = scanner.next();
             switch (cmd) {
-                case "create":
+                case "c":
                     createPerson();
                     break;
-                case "help":
+                case "h":
                     showHelp();
                     break;
-                case "showList":
+                case "l":
                     showList();
                     break;
-                case "exit":
+                case "d":
+                    deleteRecord();
+                    break;
+                case "e":
+                    System.out.println("Auf viedersehen");
                     return;
                 default:
-                    System.out.println("Wrong command");
+                    System.out.println("Wrong command. Type 'help' to see the list.");
             }
         }
     }
     private static void showList() {
-
+        records.forEach(r -> System.out.println(r));
     }
+
     private static void showHelp() {
+        System.out.println("List of available commands:");
+        System.out.println("create: adds person to ");
+        System.out.println("list: shows list of persons");
+        System.out.println("delete: delete person with given id");
+        System.out.println("help: shows current help ");
 
     }
     private static void createPerson() {
+        Person person = new Person();
 
+        person.setName(askString("Enter person's name:"));
+        person.setSurname(askString("Enter person's surname:"));
+        person.setPhone(validatePhone());
+        person.setEmail(askString("Enter person's e-mail:"));
+
+        records.add(person);
+    }
+
+    private static String askString(String msg) {
+        String returnedString;
+        while (true) {
+            System.out.println(msg);
+            returnedString = scanner.next();
+            if (returnedString.startsWith("\"")) {
+                List<String> stringList = getValuesList(returnedString);
+                returnedString = String.join(" ", stringList);
+            }
+            if (returnedString.length() <= 1) {
+                System.out.println("Entered value is too short");
+            } else {
+                break;
+            }
+        }
+
+        return returnedString;
+    }
+
+    private static List<String> getValuesList(String returnedString) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add(returnedString);
+        String tmpString;
+        do {
+            tmpString = scanner.next();
+            stringList.add(tmpString);
+        }
+        while (!tmpString.endsWith("\""));
+        return stringList;
+    }
+
+
+    private static void deleteRecord() {
+        System.out.println("Enter ID of record You would like to delete");
+        int enteredID = getEnteredNumber();
+        Person personTodelete = null;
+
+        for(Person person : records){
+            if (person.getId() == enteredID) {
+                personTodelete = person;
+            }
+        }
+        if (personTodelete != null) {
+            records.remove(personTodelete);
+        }
+    }
+
+    private static int getEnteredNumber() {
+        while(true) {
+            try {
+                int enteredNumber = scanner.nextInt();
+
+                if (Optional.empty().equals(records.stream().filter(person -> person.getId() == enteredNumber).findFirst())) {
+                    System.out.println("Error! current ID is not presented, please check list.");
+                    continue;
+                }
+                return enteredNumber;
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.println("Please enter a number. Cheater durackij. " + e);
+            }
+        }
+
+    }
+
+    private static String validatePhone() {
+        String phoneNumber = askString("Enter person's phone:");
+        while(phoneNumber.length() <5) {
+            System.out.println("Phone number is too short, enter at least 5 symbols");
+            phoneNumber = askString("Enter person's phone:");
+        }
+        return phoneNumber;
     }
 }
--- src/lv/ctco/notepad/Person.java
+++ src/lv/ctco/notepad/Person.java
@@ -4,5 +4,62 @@ package lv.ctco.notepad;
  * Created by m.troushnikova on 11/16/2018.
  */
 public class Person {
-    String name;
+    private static int counter = 0;
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+
+    public Person() {
+        counter++;
+        id = counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
